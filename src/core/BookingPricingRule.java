9769ec22404e3dc1f035d60affcd452098080c10
sereni <sereni.nm@gmail.com>
2016-12-06 14:37:19 +0000

    Add public constructor to BookingPricingRule

--- src/core/BookingPricingRule.java
+++ src/core/BookingPricingRule.java
@@ -6,6 +6,11 @@ import java.util.Date;
 public class BookingPricingRule extends HotelPricingRule {
 	private ArrayList<Date> dates;
 
+	public BookingPricingRule(float multiplier, ArrayList<Date> dates) {
+		this.dates = dates;
+		this.multiplier = multiplier;
+	}
+
 	@Override
 	public float getMultiplier(Date date) {
 		//TODO: check if date is in dates array --> return multiplier
